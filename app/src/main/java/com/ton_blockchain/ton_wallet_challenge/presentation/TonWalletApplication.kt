557b9721e2146dd7981136cbe262288e5fe039dd
Mahsa Alamshahi <alamshahi.mahsa@gmail.com>
2023-04-24 17:28:14 +0330

    MultidexApplication dependency added.

--- app/src/main/java/com/ton_blockchain/ton_wallet_challenge/presentation/TonWalletApplication.kt
+++ app/src/main/java/com/ton_blockchain/ton_wallet_challenge/presentation/TonWalletApplication.kt
@@ -1,8 +1,8 @@
 package com.ton_blockchain.ton_wallet_challenge.presentation
 
-import android.app.Application
+import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
 
 
 @HiltAndroidApp
-class TonWalletApplication: Application() {}
+class TonWalletApplication: MultiDexApplication() {}
