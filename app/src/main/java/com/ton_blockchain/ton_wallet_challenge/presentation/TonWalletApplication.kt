20cc54112fd321f212d7320a76be89a581f30907
Mahsa Alamshahi <alamshahi.mahsa@gmail.com>
2023-04-24 17:22:47 +0330

    TonWalletApplication added.

---
+++ app/src/main/java/com/ton_blockchain/ton_wallet_challenge/presentation/TonWalletApplication.kt
@@ -0,0 +1,8 @@
+package com.ton_blockchain.ton_wallet_challenge.presentation
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class TonWalletApplication: Application() {}
