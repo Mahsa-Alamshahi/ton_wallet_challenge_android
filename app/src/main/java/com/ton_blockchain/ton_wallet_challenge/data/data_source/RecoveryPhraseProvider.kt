13d50860652017ca13f77c23c99297f465813ff7
Mahsa Alamshahi <alamshahi.mahsa@gmail.com>
2023-04-25 12:06:11 +0330

    RecoveryPhraseGenerator usecase added.

--- app/src/main/java/com/ton_blockchain/ton_wallet_challenge/data/data_source/RecoveryPhraseProvider.kt
+++ app/src/main/java/com/ton_blockchain/ton_wallet_challenge/data/data_source/RecoveryPhraseProvider.kt
@@ -8,13 +8,12 @@ class RecoveryPhraseProvider {
     fun generateRecoveryPhrase(): Map<Int, String> {
         val recoveryPhraseMap = mutableMapOf<Int, String>()
         val mnemonicCode: Mnemonics.MnemonicCode = Mnemonics.MnemonicCode(Mnemonics.WordCount.COUNT_24)
-        var index: Int = 1
+        var index = 1
         for (word in mnemonicCode) {
             recoveryPhraseMap[index] = word
             println("$index $word")
             index++
         }
-
         return recoveryPhraseMap
     }
 }
---
+++ app/src/main/java/com/ton_blockchain/ton_wallet_challenge/domain/use_case/recovery_phrase_use_case/RecoveryPhraseGeneratorUseCase.kt
@@ -0,0 +1,9 @@
+package com.ton_blockchain.ton_wallet_challenge.domain.use_case.recovery_phrase_use_case
+
+import com.ton_blockchain.ton_wallet_challenge.domain.repository.RecoveryPhraseRepository
+import javax.inject.Inject
+
+class RecoveryPhraseGeneratorUseCase @Inject constructor(private val recoveryPhraseRepository: RecoveryPhraseRepository) {
+
+    operator fun invoke(): Map<Int, String> = recoveryPhraseRepository.generateRecoveryPhrase()
+}
